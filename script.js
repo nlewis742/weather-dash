25d2413181f38cd63675fbae674ce68521185361
Natasha <nlackas12@gmail.com>
2023-01-31 12:02:24 -0600

    dayjs for days of week

--- script.js
+++ script.js
@@ -7,19 +7,33 @@
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
+var d1 = document.getElementById("d1")
+var d2 = document.getElementById("d2")
+var d3 = document.getElementById("d3")
+var d4 = document.getElementById("d4")
+var d5 = document.getElementById("d5")
 
 var today = dayjs();
-
+var futureDay = [];
+console.log(futureDay);
+$('#d1').text(futureDay[1]);
 // var today = dayjs();
 $('#currentDay').text(today.format('MMM D, YYYY'));
 
 // $('#d1').text()
 
-for (let i = 0; i<6; i++) {
+for (let i = 0; i<5; i++) {
     let nextDay = dayjs(today).add(i, "day");
     console.log(nextDay.format("YYYY-MM-DD"));
-     
+    futureDay.push(nextDay);
+    
 }
 
 
 
+
+// function storeHigh(userInput) {
+//     var scoresarray = JSON.parse(localStorage.getItem("scoresarray"))||[];
+//     scoresarray.push(userInput);
+//     localStorage.setItem("scoresarray", JSON.stringify(scoresarray));
+// }
