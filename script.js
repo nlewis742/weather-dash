203c44d25f210858852824eb3f4957068e2ccbef
Natasha <nlackas12@gmail.com>
2023-01-31 15:46:47 -0600

    adding saved cities

--- script.js
+++ script.js
@@ -7,33 +7,77 @@
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
-var d1 = document.getElementById("d1")
-var d2 = document.getElementById("d2")
-var d3 = document.getElementById("d3")
-var d4 = document.getElementById("d4")
-var d5 = document.getElementById("d5")
+var d1 = document.getElementById("d1");
+var d2 = document.getElementById("d2");
+var d3 = document.getElementById("d3");
+var d4 = document.getElementById("d4");
+var d5 = document.getElementById("d5");
+var recentSearches = document.getElementById("past-searches");
+var search = document.getElementById("search")
+var userInput = document.getElementById("searched-text")
 
 var today = dayjs();
 var futureDay = [];
-console.log(futureDay);
-$('#d1').text(futureDay[1]);
-// var today = dayjs();
-$('#currentDay').text(today.format('MMM D, YYYY'));
+var searched = [];
+
 
 // $('#d1').text()
 
-for (let i = 0; i<5; i++) {
-    let nextDay = dayjs(today).add(i, "day");
-    console.log(nextDay.format("YYYY-MM-DD"));
-    futureDay.push(nextDay);
-    
+for (let i = 0; i <= 5; i++) {
+  let nextDay = dayjs(today).add(i, "day");
+  console.log(nextDay.format("YYYY-MM-DD"));
+  futureDay.push(nextDay);
 }
 
+console.log(futureDay);
+$("#d1").text(futureDay[1]);
+$("#d2").text(futureDay[2]);
+$("#d3").text(futureDay[3]);
+$("#d4").text(futureDay[4]);
+$("#d5").text(futureDay[5]);
+$("#currentDay").text(today.format("MMM D, YYYY"));
 
+function renderSearched() {
+  recentSearches.innerHTML = "";
+  
+  for (var i = 0; i < searched.length; i++) {
+    var city = searched[i];
 
+    var li = document.createElement("li");
+    li.textContent = city;
+    li.setAttribute("data-index", i)
 
-// function storeHigh(userInput) {
-//     var scoresarray = JSON.parse(localStorage.getItem("scoresarray"))||[];
-//     scoresarray.push(userInput);
-//     localStorage.setItem("scoresarray", JSON.stringify(scoresarray));
-// }
+    recentSearches.appendChild(li);
+  }
+}
+
+function init() {
+  var storedCities = JSON.parse(localStorage.getItem("searched"));
+
+  if (storedCities !== null) {
+    searched = storedCities;
+  }
+
+  renderSearched();
+}
+
+function searchedCities() {
+localStorage.setItem("searched", JSON.stringify(searched));
+}
+
+search.addEventListener("click", function(event) {
+  event.preventDefault ();
+
+  var city = userInput.value.trim();
+console.log(city);
+  if (city === "") {
+    return;
+  }
+
+searched.push(searched)
+userInput.value = "";
+
+searchedCities ()
+renderSearched ()
+
+});
