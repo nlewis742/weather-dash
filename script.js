2cc81d0a31e48718352505840475c305a859a201
Natasha <nlackas12@gmail.com>
2023-01-31 09:54:32 -0600

    adding current and future dates

--- script.js
+++ script.js
@@ -9,3 +9,23 @@
 // THEN I am again presented with current and future conditions for that city
 
 var today = dayjs();
+
+var today = dayjs();
+$('#currentDay').text(today.format('MMM D, YYYY'));
+
+$('#d1').text()
+
+var weekday = require('dayjs/plugin/weekday')
+
+dayjs.extend(weekday)
+
+// when Sunday is the first day of the week
+dayjs().weekday(7); // next Sunday
+
+// when Monday is the first day of the week
+dayjs().weekday(7) // next Monday
+display.console.log(weekday);
+
+// when Sunday is the first day of the week
+
+dayjs().weekday(5) // next Friday (5th day after Sunday)
