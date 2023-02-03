a40dcb0a961089076241ec6fc610393b2212fbeb
Natasha <nlackas12@gmail.com>
2023-02-03 11:34:46 -0600

    working on api

--- script.js
+++ script.js
@@ -19,14 +19,14 @@ var userInput = document.getElementById("searched-text")
 var today = dayjs();
 var futureDay = [];
 var searched = [];
-
+console.log(searched);
 
 // $('#d1').text()
 
 for (let i = 0; i <= 5; i++) {
   let nextDay = dayjs(today).add(i, "day");
   console.log(nextDay.format("YYYY-MM-DD"));
-  futureDay.push(nextDay);
+  futureDay.push(nextDay.format("YYYY-MM-DD"));
 }
 
 console.log(futureDay);
@@ -37,28 +37,42 @@ $("#d4").text(futureDay[4]);
 $("#d5").text(futureDay[5]);
 $("#currentDay").text(today.format("MMM D, YYYY"));
 
-function renderSearched() {
-  recentSearches.innerHTML = "";
+// function renderSearched() {
+//   recentSearches.innerHTML = "";
   
-  for (var i = 0; i < searched.length; i++) {
-    var city = searched[i];
+//   for (var i = 0; i < searched.length; i++) {
+//     var city = searched[i];
 
-    var li = document.createElement("li");
-    li.textContent = city;
-    li.setAttribute("data-index", i)
+//     var li = document.createElement("li");
+//     li.textContent = city;
+//     // li.setAttribute("data-index", i)
+//     console.log(city);
 
-    recentSearches.appendChild(li);
-  }
-}
+//     recentSearches.appendChild(li);
+
+//    }
+// }
 
 function init() {
   var storedCities = JSON.parse(localStorage.getItem("searched"))||[];
 
   if (storedCities !== null) {
     searched = storedCities;
+    console.log(searched);
   }
 
-  renderSearched(storedCities);
+  for (var i = 0; i < searched.length; i++) {
+    var city = searched[i];
+
+    var li = document.createElement("li");
+    li.textContent = city;
+    // li.setAttribute("data-index", i)
+    console.log(city);
+
+    recentSearches.appendChild(li);
+
+  // renderSearched(storedCities);
+}
 }
 
 function searchedCities() {
@@ -76,10 +90,33 @@ console.log(city);
   }
 
 searched.push(city)
+console.log(searched);
 userInput.value = "";
 
 searchedCities ();
-renderSearched ();
+// renderSearched ();
 
 });
 
+init ()
+
+
+// below is an example of a fetch function for an api
+// function getApi() {
+//   // replace `octocat` with anyone else's GitHub username
+//   var requestUrl = 'https://api.github.com/users/octocat/repos';
+
+//   fetch(requestUrl)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       for (var i = 0; i < data.length; i++) {
+//         var listItem = document.createElement('li');
+//         listItem.textContent = data[i].html_url;
+//         repoList.appendChild(listItem);
+//       }
+//     });
+// }
+
+// fetchButton.addEventListener('click', getApi);
